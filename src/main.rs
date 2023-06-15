b9b361d10710fe924f6cce7e9c2da79c7812ad74
Simon Bugge Siggard <sisi@bizzkit.com>
2023-06-15 08:46:46 +0200

    add inital cli args

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,40 @@ mod montt;
 
 use crate::montt::Montt;
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about=None)]
 struct Cli {
-    file: String,
+    /// The .montt project description file to use.
+    #[arg(short, long, default_value = "project.montt")]
+    project: String,
+
+    /// Don't forecast, even if estimate quantiles are provided.
+    #[arg(long, default_value = "false")]
+    no_forecast: bool,
+
+    /// The number of random samples to take when forecasting.
+    #[arg(short = 'n', long, default_value = "1000000")]
+    sample_size: usize,
+
+    /// The output format to use.
+    #[arg(short, long, default_value = "json")]
+    output: Output,
 }
 
-fn main() {
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Output {
+    /// Output a Gantt chart in the form of a mermaid diagram.
+    Mermaid,
+    /// Output an HTML document containing a sankey representation of the forecast.
+    Sankey,
+    /// Output the forecast as a JSON document.
+    Json,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
-    let montt = Montt::parse(&cli.file).unwrap();
+    let montt = Montt::parse(&cli.project)?;
+    Ok(())
 }
